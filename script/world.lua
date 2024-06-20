e6ee2422177f8b2df21a91d68c62679626846229
Sir Code <taylorschneider07@yahoo.com>
2024-06-20 17:51:45 -0400

    Some attempts at fixing things

--- script/world.lua
+++ script/world.lua
@@ -1,13 +1,19 @@
 function init()
-    activeList = FindEntities("activebodies", true)
+    activeList = FindBodies("activebodies", true)
     
 end
 
 function update()
-    -- for tag,handle in activeList do
-    --     SetBodyActive(handle, true)
-    -- end
+    for tag,handle in pairs(activeList) do
+        SetBodyActive(handle, true)
+    end
 end
 
+-- Thanks to lociee on discord for figuring out height-adjustment
 function tick()
+    local cameraTransform = GetPlayerCameraTransform()
+    local worldOffset = VecAdd(cameraTransform.pos, Vec(0, 0.4, 0))
+    local cameraOffsetTransform = TransformToLocalTransform(cameraTransform, Transform(worldOffset, cameraTransform.rot))
+    
+    SetCameraOffsetTransform(cameraOffsetTransform)
 end
