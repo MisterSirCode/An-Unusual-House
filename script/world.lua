071a608c87ad0013e7c5dae98489a5b7ac2d074e
Sir Code <taylorschneider07@yahoo.com>
2024-06-20 15:54:21 -0400

    Some minor changes

---
+++ script/world.lua
@@ -0,0 +1,13 @@
+function init()
+    activeList = FindEntities("activebodies", true)
+    
+end
+
+function update()
+    -- for tag,handle in activeList do
+    --     SetBodyActive(handle, true)
+    -- end
+end
+
+function tick()
+end
