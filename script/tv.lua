ed8d0a184bf26aa1fcfe64229ba69474f86fcc00
Sir Code <taylorschneider07@yahoo.com>
2024-06-24 23:01:52 -0400

    Fix fridge doors physics

--- script/tv.lua
+++ script/tv.lua
@@ -1,5 +1,5 @@
 function draw()
     UiPush()
-
+    
     UiPop()
 end
--- script/world.lua
+++ script/world.lua
@@ -1,19 +1,20 @@
 function init()
     activeList = FindBodies("activebodies", true)
-    
+    DebugPrint(activeList)
 end
 
-function update()
+function tick()
     for tag,handle in pairs(activeList) do
         SetBodyActive(handle, true)
     end
 end
 
 -- Thanks to lociee on discord for figuring out height-adjustment
-function tick()
-    local cameraTransform = GetPlayerCameraTransform()
-    local worldOffset = VecAdd(cameraTransform.pos, Vec(0, 0.4, 0))
-    local cameraOffsetTransform = TransformToLocalTransform(cameraTransform, Transform(worldOffset, cameraTransform.rot))
+-- TODO: Fix grab position
+-- function tick()
+--     local cameraTransform = GetPlayerCameraTransform()
+--     local worldOffset = VecAdd(cameraTransform.pos, Vec(0, 0.4, 0))
+--     local cameraOffsetTransform = TransformToLocalTransform(cameraTransform, Transform(worldOffset, cameraTransform.rot))
     
-    SetCameraOffsetTransform(cameraOffsetTransform)
-end
+--     SetCameraOffsetTransform(cameraOffsetTransform)
+-- end
