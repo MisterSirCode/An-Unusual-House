1088225dfd3418f91510421704e18a9fb8f0bcbc
Sir Code <taylorschneider07@yahoo.com>
2024-06-25 23:44:40 -0400

    fix lights and remove buggy bodies

--- script/slb.lua
+++ script/slb.lua
@@ -1,10 +1,15 @@
 function init()
-    host = FindShape('host')
+    hosts = FindShapes('host')
     light = FindLight('electric')
 end
 
 function tick()
-    if IsShapeBroken(host) then
-        SetLightEnabled(light, false)
+    if IsLightActive(light) then -- Cull after light is broken
+        for i=1, #hosts do
+            local host = hosts[i]
+            if IsShapeBroken(host) then
+                SetLightEnabled(light, false)
+            end
+        end
     end
 end
