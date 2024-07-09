c53d25b93410d709469edf6eff083cff621c3197
Sir Code <taylorschneider07@yahoo.com>
2024-07-09 00:03:01 -0400

    Experimental oven script

---
+++ script/oven.lua
@@ -0,0 +1,68 @@
+function init()
+    topState = false
+    ovenState = false
+    topHeat = 0
+    ovenHeat = 0
+    topButton = FindShape('topButton')
+    topElements = FindShapes('topElement')
+    ovenButton = FindShape('ovenButton')
+    topTrigger = FindTrigger('topBurner')
+    SetTag(topButton, 'interact', 'Stove Top')
+    SetTag(ovenButton, 'interact', 'Oven')
+end
+
+function checkVal(val)
+    if val >= 1 then return 1
+    elseif val <= 0 then return 0
+    else return val end
+end
+
+function adjustVal(val, adder, state)
+    if state then return checkVal(val + adder)
+    else return checkVal(val - adder) end
+end
+
+function active(val)
+    if val < 1 or val > 0 then return true
+    else return false end
+end
+
+function round(num)
+    if math.abs(num) > 2^52 then
+      return num
+    end
+    return num < 0 and num - 2^52 + 2^52 or num + 2^52 - 2^52
+end  
+
+function tick(dt)
+    local ishape = GetPlayerInteractShape()
+    local interacted = ishape ~= 0 and InputPressed('interact')
+    if interacted and ishape == topButton then
+        topState = not topState
+    elseif interacted and ishape == ovenButton then
+        ovenState = not ovenState
+    end
+    topHeat = adjustVal(topHeat, 1 / 10 * dt, topState)
+    ovenHeat = adjustVal(ovenHeat, 1 / 10 * dt, ovenState)
+    DebugWatch('Stove Top Heat', topHeat)
+    DebugWatch('Oven Heat', ovenHeat)
+
+    if active(topHeat) then
+        DebugWatch('Working. Elements ', #topElements)
+        for i = 0, #topElements do
+            local element = topElements[i]
+            SetShapeEmissiveScale(element, topHeat)
+        end
+    end
+    if topHeat > 0 then
+        local min, max = GetTriggerBounds(topTrigger)
+        local shapes = QueryAabbShapes(min, max)
+        DebugWatch('burnables', #shapes)
+        for i = 0, #shapes do
+            local shape = shapes[i]
+            local pos = GetShapeWorldTransform(shape).pos
+            DebugWatch('burning at', pos)
+            AddHeat(shape, pos, topHeat * 2 * dt)
+        end
+    end
+end
